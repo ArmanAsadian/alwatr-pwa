69eb0c8495b6cc4107825cdadd4e4e89ef216c7d
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-13 02:49:02 +0330

    feat(alwatr-pwa): update to use alwatr element and router

--- src/alwatr-pwa.ts
+++ src/alwatr-pwa.ts
@@ -1,18 +1,36 @@
-import {css, html, LitElement} from 'lit';
+import {css, html} from 'lit';
 import {customElement} from 'lit/decorators/custom-element.js';
-import type {TemplateResult} from 'lit';
+import {initialRouter} from '@alwatr/router';
+import {AlwatrElement} from './alwatr-debt/alwatr-element';
+import type {TemplateResult, PropertyValues} from 'lit';
 
+/**
+ * Alwatr PWA Root Element
+ *
+ * ```html
+ * <alwatr-pwa></alwatr-pwa>
+ * ```
+ */
 @customElement('alwatr-pwa')
-export class AlwatrPwa extends LitElement {
-  static override styles = css`
-    :host {
-      display: block;
-    }
-  `;
+export class AlwatrPwa extends AlwatrElement {
+  static override styles = [
+    AlwatrElement.styles,
+    css`
+    `,
+  ];
 
   override render(): TemplateResult {
     return html`
       <h2>Hello world</h2>
+      <a href="/home">home</a>
+      <a href="/about">about</a>
+      <a href="/articles">articles</a>
+      <a href="/contact">contact</a>
     `;
+  }
+
+  protected override firstUpdated(_changedProperties: PropertyValues): void {
+    super.firstUpdated(_changedProperties);
+    initialRouter();
   }
 }
