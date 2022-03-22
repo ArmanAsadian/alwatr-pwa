a36c702b44d4340e3042f1433f745a50dcbf97b7
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-22 17:04:30 +0430

    feat: impeliment the router

--- src/alwatr-pwa.ts
+++ src/alwatr-pwa.ts
@@ -1,8 +1,10 @@
 import {css, html} from 'lit';
 import {customElement} from 'lit/decorators/custom-element.js';
-import {initialRouter} from '@alwatr/router';
+import {router} from '@alwatr/router';
 import {AlwatrElement} from './alwatr-debt/alwatr-element';
-import type {TemplateResult, PropertyValues} from 'lit';
+import type {ListenerInterface} from '@alwatr/signal';
+import type {RoutesConfig} from '@alwatr/router';
+import type {TemplateResult} from 'lit';
 
 /**
  * Alwatr PWA Root Element
@@ -19,18 +21,56 @@ export class AlwatrPwa extends AlwatrElement {
     `,
   ];
 
-  override render(): TemplateResult {
-    return html`
-      <h2>Hello world</h2>
-      <a href="/home">home</a>
-      <a href="/about">about</a>
-      <a href="/articles">articles</a>
-      <a href="/contact">contact</a>
-    `;
+  private _routes: RoutesConfig = {
+    map: (route) => route.sectionList[0]?.toString(),
+    list: {
+      'home': {
+        render: () => html`<page-home>Page Home ...</page-home>`,
+      },
+      'about': {
+        render: () => html`<page-about>Page About ...</page-about>`,
+      },
+      'article': {
+        render: (route) =>
+          route.sectionList[1] != null ?
+            html`<page-article>Page Article ${route.sectionList[1]} ...</page-article>` :
+            this._routes.list['404'],
+      },
+    },
+  };
+
+
+  constructor() {
+    super();
+    router.initial();
   }
 
-  protected override firstUpdated(_changedProperties: PropertyValues): void {
-    super.firstUpdated(_changedProperties);
-    initialRouter();
+  private _listenerList: Array<unknown> = [];
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+    this._listenerList.push(router.signal.addListener(() => this.requestUpdate()));
+  }
+
+  override disconnectedCallback(): void {
+    super.disconnectedCallback();
+    this._listenerList.forEach((listener) => (listener as ListenerInterface<keyof AlwatrSignals>).remove());
+  }
+
+  override render(): TemplateResult {
+    return html`
+      <h2>Hello World!</h2>
+
+      <menu>
+        <li><a href=${router.makeUrl({sectionList: ['home']})}>Home</a></li>
+        <li><a href=${router.makeUrl({sectionList: ['about']})}>About</a></li>
+        <li><a href=${router.makeUrl({sectionList: ['article', 100]})}>Article 100</a></li>
+        <li><a href=${router.makeUrl({sectionList: ['contact']})}>Contact</a></li>
+      </menu>
+
+      <div class="page-container">
+        ${router.outlet(this._routes)}
+      </div>
+    `;
   }
 }
