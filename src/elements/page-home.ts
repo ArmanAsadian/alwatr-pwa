273fae3d466afb82efe0699ef70d82c02282f37a
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-24 02:24:13 +0430

    feat(page-home): improve layout

--- src/elements/page-home.ts
+++ src/elements/page-home.ts
@@ -44,9 +44,9 @@ export class PageHome extends AlwatrElement {
       --padding-bottom: var(--page-content-padding);
     }
 
-    .grid-container {
+    .menu-container {
       display: flex;
-      flex-wrap: wrap;
+      flex-direction: column;
       gap: 8px;
     }
 
@@ -60,7 +60,7 @@ export class PageHome extends AlwatrElement {
       background-size: cover;
       background-position: center center;
       width: 100%;
-      height: 42vw;
+      height: 35vw;
       border-radius: 4px;
       box-shadow: rgba(0, 0, 0, 12%) 0px 4px 16px;
     }
@@ -72,6 +72,22 @@ export class PageHome extends AlwatrElement {
       padding: 0 14px;
       border-radius: 8px;
       font-size: 20px;
+    }
+
+    .cart-group {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 8px;
+    }
+
+    .cart-group .cart-image {
+      flex-grow: 1;
+      flex-shrink: 1;
+      flex-basis: 40%;
+    }
+
+    .cart-group .cart-image h2 {
+      font-size: 16px;
     }
   `;
 
@@ -96,24 +112,18 @@ export class PageHome extends AlwatrElement {
       </ion-header>
 
       <ion-content class="padding">
-        <div class="grid-container">
+        <div class="menu-container">
           <a
             class="cart-image"
             href=${router.makeUrl({sectionList: ['aaa']})}
-            style="background-image: url('/images/1005.jpeg');">
+            style="background-image: url('/images/1005.jpeg'); height: 30vw;">
             <h2 style="background-color: rgba(160, 122, 75, 90%);">احمد کیست؟</h2>
           </a>
           <a
             class="cart-image"
             href=${router.makeUrl({sectionList: ['aaa']})}
-            style="background-image: url('/images/1014.jpeg');">
+            style="background-image: url('/images/1014.jpeg'); height: 50vw;">
             <h2 style="background-color: rgba(122, 174, 185, 90%);">باورها و اعتقادات</h2>
-          </a>
-          <a
-            class="cart-image"
-            href=${router.makeUrl({sectionList: ['aaa']})}
-            style="background-image: url('/images/1025.jpeg');">
-            <h2 style="background-color: rgba(171, 185, 80, 90%);">عنوان نمونه</h2>
           </a>
           <a
             class="cart-image"
@@ -121,30 +131,40 @@ export class PageHome extends AlwatrElement {
             style="background-image: url('/images/1010.jpeg');">
             <h2 style="background-color: rgba(60, 78, 83, 90%);">مقالات</h2>
           </a>
+          <div class="cart-group">
+            <a
+              class="cart-image"
+              href=${router.makeUrl({sectionList: ['aaa']})}
+              style="background-image: url('/images/1078.jpeg');">
+              <h2 style="background-color: rgba(69, 50, 83, 0.9);">ویدیوها</h2>
+            </a>
+            <a
+              class="cart-image"
+              href=${router.makeUrl({sectionList: ['aaa']})}
+              style="background-image: url('/images/1059.jpeg');">
+              <h2 style="background-color: rgba(166, 138, 121, 90%);">پادکست</h2>
+            </a>
+          </div>
           <a
             class="cart-image"
             href=${router.makeUrl({sectionList: ['aaa']})}
-            style="background-image: url('/images/1078.jpeg');">
-            <h2 style="background-color: rgba(69, 50, 83, 0.9);">ویدیوها</h2>
+            style="background-image: url('/images/1025.jpeg');">
+            <h2 style="background-color: rgba(171, 185, 80, 90%);">عنوان نمونه</h2>
           </a>
-          <a
-            class="cart-image"
-            href=${router.makeUrl({sectionList: ['aaa']})}
-            style="background-image: url('/images/1059.jpeg');">
-            <h2 style="background-color: rgba(166, 138, 121, 90%);">پادکست</h2>
-          </a>
-          <a
-            class="cart-image"
-            href=${router.makeUrl({sectionList: ['aaa']})}
-            style="background-image: url('/images/1071.jpeg');">
-            <h2 style="background-color: rgba(28, 117, 120, 0.9);">وب‌سایت‌ها</h2>
-          </a>
-          <a
-            class="cart-image"
-            href=${router.makeUrl({sectionList: ['aaa']})}
-            style="background-image: url('/images/1067.jpeg');">
-            <h2 style="background-color: rgba(176, 134, 105, 0.9);">شبکه‌های اجتماعی</h2>
-          </a>
+          <div class="cart-group">
+            <a
+              class="cart-image"
+              href=${router.makeUrl({sectionList: ['aaa']})}
+              style="background-image: url('/images/1071.jpeg');">
+              <h2 style="background-color: rgba(28, 117, 120, 0.9);">وب‌سایت‌ها</h2>
+            </a>
+            <a
+              class="cart-image"
+              href=${router.makeUrl({sectionList: ['aaa']})}
+              style="background-image: url('/images/1067.jpeg');">
+              <h2 style="background-color: rgba(176, 134, 105, 0.9);">شبکه‌های اجتماعی</h2>
+            </a>
+          </div>
         </div>
       </ion-content>
     `;
